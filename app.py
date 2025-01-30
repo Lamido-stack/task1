20e5377578744bdc4a1a463810dd7754bfcfe6b9
lamido <uklamido@gmail.com>
2025-01-30 18:27:57 +0100

    sec

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import os
 from flask import Flask, jsonify
 from datetime import datetime, timezone
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/info', methods=['GET'])
 def get_info():
@@ -17,4 +20,5 @@ def get_info():
     return jsonify(data), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
